d180b51d8544e1d4f17f59e4ae507ff229b0944d
weaselmc <mbuttsworth@hotmail.com>
2026-07-06 18:37:58 +0800

    fixed packing trying original function for tas-parser

--- function_app.py
+++ function_app.py
@@ -1,6 +1,8 @@
 
 
 import traceback
+import json
+import base64
 
 try:
     import tas_parser
@@ -10,19 +12,49 @@ except Exception as ex:
     IMPORT_ERROR = traceback.format_exc()
 
 import azure.functions as func
-
-try:
-    from docx import Document
-    STATUS = "docx imported OK"
-except Exception as ex:
-    STATUS = str(ex)
+from tas_parser import TASDoc
 
 app = func.FunctionApp()
+@app.route(
+    route="parse-tas",
+    auth_level=func.AuthLevel.FUNCTION
+)
+def parse_tas(req: func.HttpRequest) -> func.HttpResponse:
 
-@app.route(route="hello")
-def hello(req: func.HttpRequest) -> func.HttpResponse:
+    try:
+
+        body = req.get_json()
+
+        file_name = body.get("fileName")
+
+        file_content = body.get("fileContent")
+
+        file_bytes = base64.b64decode(
+            file_content
+        )
+
+        result = TASDoc(file_bytes).parse().to_lists()         
+
+        return func.HttpResponse(
+            json.dumps(
+                result,
+                default=str
+            ),
+            mimetype="application/json",
+            status_code=200
+        )
+
+    except Exception as ex:
+
+        return func.HttpResponse(
+            str(ex),
+            status_code=500
+        )
+    
+@app.route(route="test")
+def test(req: func.HttpRequest) -> func.HttpResponse:
 
     if IMPORT_OK:
-        return func.HttpResponse(f"Import OK. {STATUS}")
+        return func.HttpResponse(f"Import OK.")
     else:
         return func.HttpResponse(IMPORT_ERROR)
